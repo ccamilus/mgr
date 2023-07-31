f9e4bab6f09112dd233cd545b13f3729ac723105
ccamilus <kcichm@gmail.com>
2023-07-31 15:35:34 +0200

    Add converter for evaluation function, simplify main converter, fix main psq filenames, add psq files and csv file for evaluation function, simplify csv shifter

--- data-processing/converters.py
+++ data-processing/converters.py
@@ -1,32 +1,36 @@
-import csv
+import glob
 import os
 from pathlib import Path
 
+import csv
 import utils as u
 
+BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+BOARD_SIZE = 15
 
-class PsqToCsvConverter:
-    def _prepare_files(self, output_path, board_size):
+
+class MainConverter:
+    def _prepare_files(self):
         target_fields = []
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        oddity_bonus = 0 if board_size % 2 == 0 else 1
-        center = (board_size // 2) + oddity_bonus
+        if not os.path.exists(BASE_DIR.joinpath("csv/main")):
+            os.makedirs(BASE_DIR.joinpath("csv/main"))
+        oddity_bonus = 0 if BOARD_SIZE % 2 == 0 else 1
+        center = (BOARD_SIZE // 2) + oddity_bonus
         header_row = []
-        for i in range(1, board_size ** 2 + 1):
+        for i in range(1, BOARD_SIZE ** 2 + 1):
             header_row.append(f"0player_field{i}")
             header_row.append(f"1player_field{i}")
         header_row.append(f"player_decision")
         for i in range(1, center + 1):
             for j in range(1, i + 1):
-                field = u.transform_coordinate_to_index(j, i, board_size)
+                field = u.transform_coordinate_to_index(j, i, BOARD_SIZE)
                 target_fields.append(field)
-                with open(output_path.joinpath(f"{field}.csv"), "w", newline="") as csv_file:
+                with open(BASE_DIR.joinpath(f"csv/main/{field}.csv"), "w", newline="") as csv_file:
                     writer = csv.writer(csv_file)
                     writer.writerow(header_row)
         return target_fields
 
-    def _get_moves(self, file_directory, board_size):
+    def _get_moves(self, file_directory):
         moves = []
         with open(file_directory, "r") as file:
             data = [line.rstrip('\n') for line in file]
@@ -36,12 +40,11 @@ class PsqToCsvConverter:
             except IndexError:
                 pass
             else:
-                if board_sizes[0] == board_sizes[1] and int(board_sizes[0]) == board_size:
+                if board_sizes[0] == board_sizes[1] and int(board_sizes[0]) == BOARD_SIZE:
                     for i in range(1, len(data)):
                         line = data[i].split(",")
                         if len(line) == 3 and all(_.isdigit() for _ in line):
-                            if 0 < int(line[0]) <= board_size and \
-                                    0 < int(line[1]) <= board_size:
+                            if 0 < int(line[0]) <= BOARD_SIZE and 0 < int(line[1]) <= BOARD_SIZE:
                                 moves.append((int(line[0]), int(line[1])))
                             else:
                                 moves = None
@@ -50,7 +53,7 @@ class PsqToCsvConverter:
                         moves = None
         return moves
 
-    def _perform_symmetry(self, moves, board_size):
+    def _perform_symmetry(self, moves):
         tmp_board_state = []
         tmp_game = []
         game_after_symmetry = []
@@ -58,21 +61,20 @@ class PsqToCsvConverter:
             tmp_game.append((tmp_board_state.copy(), move))
             tmp_board_state.append(move)
         for tmp_state in tmp_game:
-            classifiers = self._get_classifiers(tmp_state[1][0], tmp_state[1][1], board_size).split(",")
+            classifiers = self._get_classifiers(tmp_state[1][0], tmp_state[1][1]).split(",")
             decision = tmp_state[1]
             state = tmp_state[0]
             for classifier in classifiers:
-                decision = self._perform_coordinates_transformation(decision[0], decision[1], classifier, board_size)
+                decision = self._perform_coordinates_transformation(decision[0], decision[1], classifier)
                 for i in range(len(state)):
-                    state[i] = self._perform_coordinates_transformation(state[i][0], state[i][1], classifier,
-                                                                        board_size)
+                    state[i] = self._perform_coordinates_transformation(state[i][0], state[i][1], classifier)
             game_after_symmetry.append((state.copy(), decision))
         return game_after_symmetry
 
-    def _get_classifiers(self, x, y, board_size):
-        board_center = board_size // 2
+    def _get_classifiers(self, x, y):
+        board_center = BOARD_SIZE // 2
         oddity_bonus = 0
-        if not board_size % 2 == 0:
+        if not BOARD_SIZE % 2 == 0:
             oddity_bonus = 1
             if y == board_center + oddity_bonus:
                 return "t" if x <= board_center + oddity_bonus else "v"
@@ -87,7 +89,7 @@ class PsqToCsvConverter:
             else:
                 return "d1"
         if x > board_center + oddity_bonus and y <= board_center:
-            if y > board_size - x:
+            if y > BOARD_SIZE - x:
                 return "v"
             else:
                 return "v,d1"
@@ -97,35 +99,35 @@ class PsqToCsvConverter:
             else:
                 return "h,v"
         if x <= board_center and y > board_center + oddity_bonus:
-            if y > board_size - x + 1:
+            if y > BOARD_SIZE - x + 1:
                 return "h,d1"
             else:
                 return "h"
 
-    def _perform_coordinates_transformation(self, x, y, classifier, board_size):
+    def _perform_coordinates_transformation(self, x, y, classifier):
         match classifier:
             case "v":
-                return board_size - x + 1, y
+                return BOARD_SIZE - x + 1, y
             case "h":
-                return x, board_size - y + 1
+                return x, BOARD_SIZE - y + 1
             case "d1":
                 return y, x
             case "d2":
-                return board_size - y + 1, board_size - x + 1
+                return BOARD_SIZE - y + 1, BOARD_SIZE - x + 1
             case "t":
                 return x, y
 
-    def _update_files(self, game_coord, output_path, board_size, target_fields):
+    def _update_files(self, game_coord, target_fields):
         game_ind = []
         for state in game_coord:
-            decision = u.transform_coordinate_to_index(state[1][0], state[1][1], board_size)
+            decision = u.transform_coordinate_to_index(state[1][0], state[1][1], BOARD_SIZE)
             current_state = []
             for i in range(len(state[0])):
-                current_state.append(u.transform_coordinate_to_index(state[0][i][0], state[0][i][1], board_size))
+                current_state.append(u.transform_coordinate_to_index(state[0][i][0], state[0][i][1], BOARD_SIZE))
             game_ind.append((current_state.copy(), decision))
         for state in game_ind:
             row = []
-            for i in range(1, (board_size ** 2) + 1):
+            for i in range(1, (BOARD_SIZE ** 2) + 1):
                 if i in state[0]:
                     if state[0].index(i) % 2 == 0:
                         row.append(1)
@@ -140,14 +142,91 @@ class PsqToCsvConverter:
                 decision_value = 1 if field == state[1] else 0
                 csv_row = row.copy()
                 csv_row.append(decision_value)
-                with open(output_path.joinpath(f"{field}.csv"), "a", newline="") as csv_file:
+                with open(BASE_DIR.joinpath(f"csv/main/{field}.csv"), "a", newline="") as csv_file:
                     writer = csv.writer(csv_file)
                     writer.writerow(csv_row)
 
-    def convert(self, psq_files_directories, output_path, board_size):
-        target_fields = self._prepare_files(Path(output_path), board_size)
+    def convert(self):
+        psq_files_directories = glob.glob(str(BASE_DIR.joinpath("psq/main/*.psq")))
+        target_fields = self._prepare_files()
         for file_directory in psq_files_directories:
-            moves = self._get_moves(file_directory, board_size)
+            moves = self._get_moves(file_directory)
             if moves:
-                game = self._perform_symmetry(moves, board_size)
-                self._update_files(game, Path(output_path), board_size, target_fields)
+                game = self._perform_symmetry(moves)
+                self._update_files(game, target_fields)
+
+
+class EvaluationConverter:
+    def _get_game_state_rows(self, moves):
+        game_states = [(moves[:i], 1 if len(moves) % 2 == 0 else 0) for i in range(8, len(moves) + 1)]
+        game_state_rows = []
+        for game_state in game_states:
+            row = []
+            for i in range(1, (BOARD_SIZE ** 2) + 1):
+                if i in game_state[0]:
+                    if game_state[0].index(i) % 2 == 0:
+                        row.append(1)
+                        row.append(0)
+                    else:
+                        row.append(0)
+                        row.append(1)
+                else:
+                    row.append(0)
+                    row.append(0)
+            row.append(game_state[1])
+            game_state_rows.append(row)
+        return game_state_rows
+
+    def _get_moves(self, file_directory):
+        moves = []
+        with open(file_directory, "r") as file:
+            data = [line.rstrip('\n') for line in file]
+        if data:
+            try:
+                board_sizes = data[0].split(",")[0].split()[1].split("x")
+            except IndexError:
+                pass
+            else:
+                if board_sizes[0] == board_sizes[1] and int(board_sizes[0]) == BOARD_SIZE:
+                    for i in range(1, len(data)):
+                        line = data[i].split(",")
+                        if len(line) == 3 and all(_.isdigit() for _ in line):
+                            if 0 < int(line[0]) <= BOARD_SIZE and 0 < int(line[1]) <= BOARD_SIZE:
+                                moves.append(u.transform_coordinate_to_index(int(line[0]), int(line[1]), BOARD_SIZE))
+                            else:
+                                moves = None
+                                break
+                    if (moves and len(moves) != len(set(moves))) or len(moves) == BOARD_SIZE ** 2:
+                        moves = None
+        return moves
+
+    def convert(self):
+        game_state_rows = []
+        psq_files_directories = glob.glob(str(BASE_DIR.joinpath("psq/evaluation/*.psq")))
+        for file_directory in psq_files_directories:
+            moves = self._get_moves(file_directory)
+            if moves:
+                game_state_rows.extend(self._get_game_state_rows(moves))
+        if game_state_rows:
+            header_row = []
+            for i in range(1, BOARD_SIZE ** 2 + 1):
+                header_row.append(f"0player_field{i}")
+                header_row.append(f"1player_field{i}")
+            header_row.append(f"winner")
+            if not os.path.exists(BASE_DIR.joinpath("csv/evaluation")):
+                os.makedirs(BASE_DIR.joinpath("csv/evaluation"))
+            with open(BASE_DIR.joinpath("csv/evaluation/evaluation.csv"), "w", newline="") as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(header_row)
+                writer.writerows(game_state_rows)
+
+
+def main():
+    mc = MainConverter()
+    mc.convert()
+    ec = EvaluationConverter()
+    ec.convert()
+
+
+if __name__ == "__main__":
+    main()
--- data-processing/shifters.py
+++ data-processing/shifters.py
@@ -4,49 +4,56 @@ from pathlib import Path
 
 import utils as u
 
+BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+BOARD_SIZE = 15
+
 
 class CsvShifter:
     @staticmethod
-    def shift(base_field_path, target_field_path, board_size):
+    def shift(base_field, target_field):
+        shifted_rows = []
+        x = u.transform_index_to_coordinate(target_field, BOARD_SIZE)[0] - \
+            u.transform_index_to_coordinate(base_field, BOARD_SIZE)[0]
+        y = u.transform_index_to_coordinate(target_field, BOARD_SIZE)[1] - \
+            u.transform_index_to_coordinate(base_field, BOARD_SIZE)[1]
         try:
-            base_field = int(Path(base_field_path).stem)
-            target_field = int(Path(target_field_path).stem)
-        except ValueError:
-            print("Filename must be an integer, eg. 113.csv.")
+            with open(BASE_DIR.joinpath(f"csv/main/{base_field}.csv"), "r") as csv_file:
+                for row_index, row in enumerate(csv_file):
+                    if row_index == 0:
+                        shifted_rows.append(row.strip().split(","))
+                    else:
+                        corrupted_fields = []
+                        not_shifted_row = list(map(int, row.strip().split(",")))
+                        decision_value = not_shifted_row[-1]
+                        not_shifted_row.pop()
+                        for column_index, column_value in enumerate(not_shifted_row):
+                            if column_value == 1:
+                                player = 0 if column_index % 2 == 0 else 1
+                                field = ((column_index - player) // 2) + 1
+                                tmp_x, tmp_y = u.transform_index_to_coordinate(field, BOARD_SIZE)
+                                if 0 < tmp_x + x <= BOARD_SIZE and 0 < tmp_y + y <= BOARD_SIZE:
+                                    corrupted_fields.append(
+                                        (u.transform_coordinate_to_index(tmp_x + x, tmp_y + y, BOARD_SIZE), player))
+                        shifted_row = [0 for _ in range((BOARD_SIZE ** 2) * 2)]
+                        shifted_row.append(decision_value)
+                        for field in corrupted_fields:
+                            shifted_row[((field[0] - 1) * 2) + field[1]] = 1
+                        shifted_rows.append(shifted_row)
+        except FileNotFoundError:
+            print(f"File with base field {base_field} not found")
         else:
-            shifted_rows = []
-            x = u.transform_index_to_coordinate(target_field, board_size)[0] - \
-                u.transform_index_to_coordinate(base_field, board_size)[0]
-            y = u.transform_index_to_coordinate(target_field, board_size)[1] - \
-                u.transform_index_to_coordinate(base_field, board_size)[1]
-            try:
-                with open(base_field_path, "r") as csv_file:
-                    for row_index, row in enumerate(csv_file):
-                        if row_index == 0:
-                            shifted_rows.append(row.strip().split(","))
-                        else:
-                            corrupted_fields = []
-                            not_shifted_row = list(map(int, row.strip().split(",")))
-                            decision_value = not_shifted_row[-1]
-                            not_shifted_row.pop()
-                            for column_index, column_value in enumerate(not_shifted_row):
-                                if column_value == 1:
-                                    player = 0 if column_index % 2 == 0 else 1
-                                    field = ((column_index - player) // 2) + 1
-                                    tmp_x, tmp_y = u.transform_index_to_coordinate(field, board_size)
-                                    if 0 < tmp_x + x <= board_size and 0 < tmp_y + y <= board_size:
-                                        corrupted_fields.append(
-                                            (u.transform_coordinate_to_index(tmp_x + x, tmp_y + y, board_size), player))
-                            shifted_row = [0 for _ in range((board_size ** 2) * 2)]
-                            shifted_row.append(decision_value)
-                            for field in corrupted_fields:
-                                shifted_row[((field[0] - 1) * 2) + field[1]] = 1
-                            shifted_rows.append(shifted_row)
-            except FileNotFoundError:
-                print(f"File {base_field_path} not found.")
-            else:
-                if not os.path.exists(Path(target_field_path).parent):
-                    os.makedirs(Path(target_field_path).parent)
-                with open(target_field_path, "w", newline="") as csv_file:
-                    writer = csv.writer(csv_file)
-                    writer.writerows(shifted_rows)
+            if not os.path.exists(BASE_DIR.joinpath(f"csv/main/shifted")):
+                os.makedirs(BASE_DIR.joinpath(f"csv/main/shifted"))
+            with open(BASE_DIR.joinpath(f"csv/main/shifted/{target_field}.csv"), "w", newline="") as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerows(shifted_rows)
+
+
+def main():
+    CsvShifter.shift(64, 80)
+    CsvShifter.shift(109, 95)
+    CsvShifter.shift(110, 96)
+
+
+if __name__ == "__main__":
+    main()
